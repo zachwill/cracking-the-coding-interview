3e2a1cd6373cb1deb30aad5da43773f1424988ce
Zach Williams <hey@zachwill.com>
2011-05-22 13:42:09 -0500

    Added a list_add function

--- 20_1_custom_add.py
+++ 20_1_custom_add.py
@@ -17,11 +17,25 @@ def custom_add(first, second):
     return custom_add(combine, carry)
 
 
+def list_add(first, second):
+    """Addition with lists."""
+    first_range = range(first)
+    second_range = xrange(second)
+    first_range.extend(second_range)
+    return len(first_range)
+
+
 def main():
+    # Binary way.
     print custom_add(0, 1)    # 1
     print custom_add(2, 3)    # 5
     print custom_add(20, 12)  # 32
     print custom_add(50, 49)  # 99
+    # With lists.
+    print list_add(0, 1)    # 1
+    print list_add(2, 3)    # 5
+    print list_add(20, 12)  # 32
+    print list_add(50, 49)  # 99
 
 if __name__ == '__main__':
     main()
