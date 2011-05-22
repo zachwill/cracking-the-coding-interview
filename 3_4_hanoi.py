1adba94c36e0b9bec9909cceaf9b72a7c354fa67
Zach Williams <hey@zachwill.com>
2011-05-22 15:54:17 -0500

    Updated docstring for problem 3.4

--- 3_4_hanoi.py
+++ 3_4_hanoi.py
@@ -3,6 +3,8 @@
 """
 Towers of Hanoi in Python.
 
+Originally from:  http://codesnippets.joyent.com/posts/show/530
+
 Notice how it takes 2^n - 1 number of moves to complete.
 To see this for yourself, run the following from the commandline:
     python 3_4_hanoi.py | wc -l
@@ -11,11 +13,11 @@ To see this for yourself, run the following from the commandline:
 
 def hanoi(n, a='A', b='B', c='C'):
     """Move n discs from a to c using b as middle."""
-    if n == 0:
+    if n is 0:
         return
-    hanoi(n-1, a, c, b)
+    hanoi(n - 1, a, c, b)
     print a, '->', c
-    hanoi(n-1, b, a, c)
+    hanoi(n - 1, b, a, c)
 
 
 def main():
