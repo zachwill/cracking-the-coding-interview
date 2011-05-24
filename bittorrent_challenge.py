72ecc132cdcb939d3dc587c8d859bac889a96d2d
Zach Williams <hey@zachwill.com>
2011-05-24 17:25:53 -0500

    Added correct solution to BitTorrent Challenge

--- bittorrent_challenge.py
+++ bittorrent_challenge.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-My solution to the BitTorrent Developer Challenge.
+My solution to the BitTorrent Developer Challenge -- takes forever to run.
 
 You have 40 bowls, all placed in a line at exact intervals of 1 meter. You
 also have 9 oranges. You wish to place all the oranges in the bowls, no
@@ -10,34 +10,20 @@ and C such that the distance between A and B is equal to the distance
 between B and C. How many ways can you arrange the oranges in the bowls?.
 """
 
-from itertools import combinations
+from itertools import combinations, imap, takewhile
 
 
-def eliminate_combos(combination):
-    """
-    Eliminate combinations that do not meet the following the requirement:
-    the absolute difference between the current value and the previous value
-    must not be the same as the current value and the next value in the
-    combination.
-    """
-    last_item = len(combination) - 1
-    for index, value in enumerate(combination):
-        if index is last_item:
-            # Successfully iterated through all elements.
-            return True
-        elif index is not 0:
-            last_val = combination[index - 1]
-            next_val = combination[index + 1]
-            if abs(value - last_val) is abs(next_val - value):
-                return False
-        else:
-            # Very first element of combination.
-            continue
+def eliminate(combo):
+    """Eliminate combinations that do not fit the criteria."""
+    for x, y, z in combo:
+        if abs(x - y) is abs(y - z):
+            return False
+    return True
 
 
 def main():
-    combos = (c for c in combinations(xrange(40), 9) if eliminate_combos(c))
-    print sum(1 for combo in combos)  # 105,993,066
+    combos = (combinations(c, 3) for c in combinations(xrange(40), 9))
+    print sum(imap(eliminate, combos))  # 7,555,794
 
 if __name__ == '__main__':
     main()
